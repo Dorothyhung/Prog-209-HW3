ccc4cc761bcc9548f24208df973868ed9c5f9fca
Dorothyhung <dorothyh0602@gmail.com>
2023-01-20 22:14:54 -0800

    wrote all functions and cleaned up code to be more efficient

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
 
 origArray=[2, 3, 4];
 
-
-
-(function (){
-    //anonymous function
-}());
-
 document.addEventListener("DOMContentLoaded", function(event) {
     //write values of origArray into the 3 textboxes
-    document.getElementById("textbox1").value=origArray[0];
-    document.getElementById("textbox2").value=origArray[1];
-    document.getElementById("textbox3").value=origArray[2];
+    for (let i = 0; i < origArray.length; i++) {
+        document.getElementById("textbox"+String(i+1)).value=origArray[i];
+    }
 
-    document.addEventListener("click", function (){
-        //anonymous function
-    }());
+    //write values of cubed values into textboxes - anonymous function
+    document.getElementById("button2").addEventListener("click", function (){
+        let cubedArray=origArray.map(cubedArray => Math.pow(cubedArray, 3));
+        for (let i = 0; i < cubedArray.length; i++) {
+            document.getElementById("textbox"+String(i+1)).value=cubedArray[i];
+        }
+    });
 
-    document.addEventListener("click", button3);
+    //write values of quartic values into textbox, reads function button3()
+    document.getElementById("button3").addEventListener("click", button3);
 })
 
-function button1(){
-    squaredArray=origArray.map(square)
-    for(let i = 0; i < squaredArray.length; i++) {
-        document.getElementById("textbox"+(i).toString).value = squaredArray[i];
+//read from html for button1 to square values in textbox
+function squarebutton(){
+    let squaredArray=origArray.map(squaredArray => Math.pow(squaredArray,2));
+    for (let i = 0; i < squaredArray.length; i++) {
+        document.getElementById("textbox"+String(i+1)).value=squaredArray[i];
     }
 }
 
-function square(oneItem) {
-    Math.pow(oneItem, 2);
-}
-
-function button3() {
-
+//calculates quartic values
+function button3(){
+    let quarticArray=origArray.map(quarticArray => Math.pow(quarticArray,4));
+    for (let i = 0; i < quarticArray.length; i++) {
+        document.getElementById("textbox"+String(i+1)).value=quarticArray[i];
+    }
 }
